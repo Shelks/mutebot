d935ae252e7a52a4a414cb1d5b492ea059a208f6
Shelks <42976411+Shelks@users.noreply.github.com>
2018-12-18 20:00:24 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require ("discord.js");
+var bot = new Discord.Client();
+bot.on("ready", function() {
+
+        bot.user.setGame("Fix Mute");
+
+})
+
+bot.login("NTI0NjQyMjY5NDMzOTU0MzA3.DvrTWw.kp3b1YZqp-YVxWZzE9y1siNcxEg");
