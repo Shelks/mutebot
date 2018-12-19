ca79b2571135798f53009cec2bc26c38caeb15fc
Shelks <42976411+Shelks@users.noreply.github.com>
2018-12-19 19:06:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,7 +9,7 @@ bot.on("ready", function() {
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
-        if (message.member.roles.has(525010407807647744)) {
+        if (message.member.roles.has(524670062213201927)) {
 
                 message.delete;
 
